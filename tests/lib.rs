21d0adcdb572b41e551963bd03a2cc99facd7f59
Justin Charette <charetjc@gmail.com>
2017-03-13 02:49:06 -0400

    Add test for new_unix_socket
    Test is marked #[ignore] until travis ci environment includes neovim
    binary.
    
    Signed-off-by: Justin Charette <charetjc@gmail.com>

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,9 +1,14 @@
 extern crate neovim_lib;
 extern crate rmp;
+extern crate tempdir;
 
 use neovim_lib::session::Session;
 use neovim_lib::neovim::Neovim;
 use neovim_lib::neovim_api::NeovimApi;
+use neovim_lib::Value;
+
+use std::process::Command;
+use tempdir::TempDir;
 
 #[ignore]
 #[test]
@@ -40,4 +45,68 @@ fn edit_test() {
     nvim.command("vsplit").unwrap();
     let windows = nvim.get_windows().unwrap();
     windows[0].set_width(&mut nvim, 10).unwrap();
+}
+
+#[cfg(unix)]
+#[ignore]
+#[test]
+fn can_connect_via_unix_socket() {
+    use std::path::Path;
+    use std::thread::sleep;
+    use std::time::{Duration, Instant};
+
+    let dir = TempDir::new("neovim-lib.test").expect("Cannot create temporary directory for test.");
+
+    let socket_path = dir.path().join("unix_socket");
+
+    let _child = Command::new("nvim")
+        .arg("--embed")
+        .env("NVIM_LISTEN_ADDRESS", &socket_path)
+        .spawn()
+        .expect("Cannot start neovim");
+
+    // wait at least 1 second for neovim to start and create socket path.
+    {
+        let start = Instant::now();
+        let one_second = Duration::from_secs(1);
+        loop {
+            sleep(Duration::from_millis(100));
+
+            if let Ok(_) = std::fs::metadata(&socket_path) {
+                break;
+            }
+
+            if one_second <= start.elapsed() {
+                panic!(format!("neovim socket not found at '{:?}'", &socket_path));
+            }
+        }
+    }
+
+
+    let mut session = Session::new_unix_socket(&socket_path)
+        .expect(&format!("Unable to connect to neovim's unix socket at {:?}",
+                         &socket_path));
+
+    session.start_event_loop();
+
+    let mut nvim = Neovim::new(session);
+
+    let servername = nvim.get_vvar("servername")
+        .expect("Error retrieving servername from neovim over unix socket");
+
+    // let's make sure the servername string and socket path string both match.
+    match servername {
+        Value::String(ref name) => {
+            if Path::new(name) != socket_path {
+                panic!(format!("Server name does not match socket path! {} != {}",
+                               name,
+                               socket_path.to_str().unwrap()));
+            }
+        }
+        _ => {
+            panic!(format!("Server name does not match socket path! {:?} != {}",
+                           servername,
+                           socket_path.to_str().unwrap()))
+        }
+    }
 }
